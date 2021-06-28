f0518165e5aaa5c8d6f94e9c346b338b7901d6ab
AnnaE <annaefitzgerald@gmail.com>
2021-06-28 11:13:06 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var likesDefinition1 = document.querySelector("#likes-definition-1");
+var likesDefinition2 = document.querySelector("#likes-definition-2");
+
+function logout(element) {
+    element.innerText ="Logout";
+}
+
+function remove(element) {
+    element.remove();
+}
+
+function addedLikes(element) {
+    alert("Ninja was liked.");
+}
+
+
+
